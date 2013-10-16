549d330fe9a1075b4a139253b37e97ac90a8f35f
ZyGen21 <zygen21@yahoo.es>
2013-10-16 19:55:14 +0200

    Primer commit

---
+++ Unidad1/src/Ejercicios/Ejercicio4.java
@@ -0,0 +1,11 @@
+package Ejercicios;
+
+public class Ejercicio4 {
+    public static void main(String[] args)
+    {
+        System.out.println("\nBuenas tardes");
+        System.out.println("\n\tSegundo mensaje");
+        System.out.println("\n\tEsto no se verá por las comillas");
+        System.out.println("\nAdiós (si arreglaste el error).");
+    }//fin main
+}
---
+++ Unidad1/src/ejemplos/Holamundo2.java
@@ -0,0 +1,9 @@
+package ejemplos;
+
+public class Holamundo2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hola Mundo!");
+	}
+}
