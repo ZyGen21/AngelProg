8170bfa8e879aa70721b09db6b210aae8a1600ef
ZyGen21 <zygen21@yahoo.es>
2013-10-17 17:59:47 +0200

    Ejercicio 3b

---
+++ Unidad1/src/Primeros_ejercicios/Ejercicio3b.java
@@ -0,0 +1,17 @@
+package Primeros_ejercicios;
+
+import java.util.Scanner;
+
+public class Ejercicio3b {
+
+	public static void main(String[] args) {
+		double radio;
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Indica el radio de círculo: ");
+		radio = teclado.nextDouble();
+		teclado.close();
+		System.out.println("El perímetro del círculo es: " + ((Math.PI)*radio)*2 + " cm");
+		System.out.println("El área del círculo es: " + (radio*radio)*(Math.PI) + " cm2");
+	}
+
+}
