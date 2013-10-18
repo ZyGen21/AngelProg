1530943c3c6252b98bab7d994850659f0f1028cd
ZyGen21 <zygen21@yahoo.es>
2013-10-18 16:56:19 +0200

    Ejercicio3e

---
+++ Unidad1/src/Primeros_ejercicios/Ejercicio3e.java
@@ -0,0 +1,20 @@
+package Primeros_ejercicios;
+
+import java.util.Scanner;
+
+/*Dados los catetos de un triángulo rectángulo, calcular su hipotenusa.*/
+
+public class Ejercicio3e {
+
+	public static void main(String[] args) {
+		double cateto1,cateto2,hipotenusa;
+		Scanner teclado = new Scanner(System.in);
+		System.out.print("Introduce la longitud del cateto 1: ");
+		cateto1 = teclado.nextDouble();
+		System.out.print("Introduce la longitud del cateto 2: ");
+		cateto2 = teclado.nextDouble();
+		hipotenusa = Math.sqrt(Math.pow(cateto1, 2)+ Math.pow(cateto2, 2));
+		System.out.println("La longitud de la hipotenusa del triángulo es:" + hipotenusa + "cm");
+	}
+
+}
