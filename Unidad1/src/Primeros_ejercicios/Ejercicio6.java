f7a49b54fc76ab91aa0d5635749a89633d892af4
ZyGen21 <zygen21@yahoo.es>
2013-10-18 18:05:19 +0200

    Ejercicio6

--- Unidad1/src/Primeros_ejercicios/Ejercicio6.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio6.java
@@ -20,18 +20,30 @@ public class Ejercicio6 {
 	v1 = ((4-2)*(5+1)/2)>2-(4+3) ;
 	System.out.println ("El primer ejemplo es: " + v1);
 	
-	v2 = (6+3)>8 && (6-1)2 < 8 || 23==8 ;
+	v2 = (6+3)>8 && (6-1) * 2 < 8 || 23==8 ;
 	System.out.println ("El primer ejemplo es: " + v2);
 	
+	X = 7;
+	Z = 2;
 	v3 = (1.0 < X) && (X < Z+7.0) ;
 	System.out.println ("El primer ejemplo es: " + v3);
 	
-	v4 = (PI * XX > Y || 2PI*X <= Z ;
+	PI = Math.PI;
+	X = 1;
+	Y = 4;
+	Z = 10;
+	v4 = (PI * Math.pow(X, 2)) > Y || 2 * PI * X <= Z ;
 	System.out.println ("El primer ejemplo es: " + v4);
 	
+	X = 1;
+	Y = 4;
+	Z = 10;
 	v5 = X>3 && Y==4 || X + Y <= Z ;
 	System.out.println ("El primer ejemplo es: " + v5);
 	
+	X = 1;
+	Y = 4;
+	Z = 10;
 	v6 = X>3 && (Y==4 || X + Y <= Z) ;
 	System.out.println ("El primer ejemplo es: " + v6);
 	}
