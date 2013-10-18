e4f7abea862e1faa54de39d9303d5ffe6dcdbb95
ZyGen21 <zygen21@yahoo.es>
2013-10-18 17:25:19 +0200

    Ejercicio5

--- Unidad1/src/Primeros_ejercicios/Ejercicio5.java
+++ Unidad1/src/Primeros_ejercicios/Ejercicio5.java
@@ -8,14 +8,17 @@ datos (Mbps), y muestre el tiempo que tardará en transmitirlo usando dicha línea
 public class Ejercicio5 {
 
 	public static void main(String[] args) {
-		double tamaño,velocidad,tiempo;
+		double tamaño,velocidad;
 		Scanner teclado = new Scanner(System.in);
-		System.out.print("Introduce el tamaño del archivo: ");
+		System.out.print("Introduce el tamaño del archivo en MegaBytes: ");
 		tamaño = teclado.nextDouble();
-		System.out.print("Introduce la velocidad de la línea: ");
+		System.out.print("Introduce la velocidad de la línea en MegaBits: ");
 		velocidad = teclado.nextDouble();
-		tiempo = ((tamaño*8)) / velocidad;
-		System.out.println("El tiempo que tardará la transferencia es:" + tiempo + "segundos");
+		velocidad = (velocidad * 1000000);
+		tamaño = tamaño * 1024 * 1024 * 8 ;
+		System.out.print("Datos: " + tamaño + " bits");
+		System.out.print("\nVelocidad: " + velocidad + " bps");
+		System.out.println("\nEl tiempo que tardará la transferencia es:" + tamaño/velocidad + "segundos");
 	teclado.close();
 	}
 
